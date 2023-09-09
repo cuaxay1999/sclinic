e310fde379ae99639e4a5201a3796bfa65872d2a
Quang Anh Đặng <cuaxay1999@gmail.com>
2023-09-09 11:39:14 +0700

    add robots.js

---
+++ src/app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://sclinic-cuaxay1999.vercel.app/sitemap.xml',
+  }
+}
